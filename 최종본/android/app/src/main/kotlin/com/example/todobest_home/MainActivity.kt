278087baae33d061a724cd7114dd1d2d6cac9ca7
Six_Tail <qowlgh1215@naver.com>
2024-06-07 22:23:17 +0900

    테마 추가

--- 최종본/android/app/src/main/kotlin/com/example/todobest_home/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.todobest_home
-
-//import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
--- 최종본/lib/utils/Main.Colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:hexcolor/hexcolor.dart';
-
-class MainColors {
-  //// #FDB082
-  static HexColor mainColor = HexColor('#FFFFFF');
-  static HexColor textColor = HexColor('#FDB082');
-}
